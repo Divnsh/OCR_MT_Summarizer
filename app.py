9f61938e1a539fc78805611244cbac51daff3098
divyansh madhwal <divyansh.madhwal@gmail.com>
2020-10-22 01:53:29 +0530

    heroku 2nd commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import flask
 from flask import send_from_directory
 import urllib
 
-
+os.system('rm -r /app/.heroku/python/lib/python3.6/site-packages/cv2/qt')
 external_stylesheets=['/assets/amyoshinopen.css']
 app = dash.Dash(__name__, title = 'Image to Doc', external_stylesheets=external_stylesheets)
 server = app.server
