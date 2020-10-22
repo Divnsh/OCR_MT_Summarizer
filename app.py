cacdf37ef65a0f7f0a3d8cadb98b18280564409a
divyansh madhwal <divyansh.madhwal@gmail.com>
2020-10-22 15:51:00 +0530

    final push

--- app.py
+++ app.py
@@ -188,4 +188,4 @@ def delayedlinks(n_clicks):
         return {'display':'inline-block','color':colors["Dark Cornflower Blue"]}
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
